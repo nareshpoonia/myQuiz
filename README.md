be49b6ba7821c354afc5162c1b802c13b1af8af8
Naresh <68911787+nareshpoonia@users.noreply.github.com>
2021-09-04 16:34:47 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Neog Camp - Quiz About Myself (Assignment for Point Three)
+
+This is a CLI app for a quiz about myself, which is also assignement for point three of NeogCamp Bootcamp
+
+## Table of contents
+
+- [About](#about)
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+### About
+
+In this app users can play a quiz about myself.
+
+
+### Links
+Both solution link and the Live Site URL to be updated soon
+- Solution URL: [Solution URL](https://github.com/nareshpoonia/b)
+- Live Site URL: [Live site URL](https:///)
+
+### Built with
+
+- Javascript
+
+### What I learned
+
+I learned the implications of putting the variable in a function instead of putting it as a global variable
+
+### Continued development
+
+
+## Author
+
+- Website - [Naresh Poonia](https://www.nareshpoonia.com)
+- Dev.to - [@nareshpoonia](https://dev.to/naresh)
+- Twitter - [@naresh*poonia*](https://twitter.com/naresh_poonia_)
+
---
+++ index.js
@@ -0,0 +1,52 @@
+// ex14: club everything to make the game
+
+let readlineSync = require ("readline-sync");
+let userName = readlineSync.question("What's your name ? ")
+console.log("Welcome "+ userName + " to HOW WELL DO YOU KNOW NARESH ?" )
+
+let score =0;
+
+function playGame(question, answer){
+  var userAnswer = readlineSync.question(question)
+  if(userAnswer===answer){
+    console.log("Right Answer")
+    score= score+1;
+  }
+  else {
+    console.log("Wrong Answer")
+    score = score
+  }
+  console.log("Your score is " + score);
+}
+// Making an array of objects containing key value pairs of questions and answers
+var qnaList = [{
+  question: "Where do i live? ",
+  answer: "Gurgaon"
+},
+{
+question:"Which branch of Engineering did i study in college? ",
+answer: "Civil Engineering"
+},
+{
+question:"How old am i? ",
+answer: "31"
+},
+{
+question:"Which bike do i ride? ",
+answer: "Hero Xpulse"
+},
+{
+question:"Where did i travelled last? ",
+answer: "Ladakh"
+}
+]
+//Making a for loop & calling function with differnt questions and answers
+
+for(let i=0; i<qnaList.length;i++){
+playGame(qnaList[i].question,qnaList[i].answer)
+}
+
+console.log("Your final score is "+ score )
+
+
+
